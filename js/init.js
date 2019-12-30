b5d23e9c73dfc8c6329dd03dee51956b0822c00d
sophiaty <sophiaty.0315@gmail.com>
2019-12-30 15:20:36 -0500

    fix typo

--- js/init.js
+++ js/init.js
@@ -514,7 +514,7 @@ function arlo_tm_animate_text(){
 	var animateSpan			= jQuery('.arlo_tm_animation_text_word');
 	
 		animateSpan.typed({
-			strings: ["Researcher", "Instructor", "Data Scientist", "Software Developer"],
+			strings: ["a Researcher", "an Instructor", "a Data Scientist", "a Software Developer"],
 			loop: true,
 			startDelay: 1e3,
 			backDelay: 2e3
